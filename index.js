0c605ce105967dd3d9b27a7067b672379c1bb51a
Hartha Aloufi <hartha.aloufee@econstruct.ai>
2023-09-20 01:17:05 +0300

    Feat: handle post not found error

--- index.js
+++ index.js
@@ -44,13 +44,37 @@ const createPost = (postId, title, message) => {
     `
 }
 
+const createNotFoundMessage = () => {
+    return `<div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+  <strong class="font-bold">404!</strong>
+  <span class="block sm:inline">Post not found.</span>
+  <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+  </span>
+</div>`
+}
+
 /**
  * Fetch post and comments from API and render them
  * @param {number} postsId 
  */
 const renderPost = async (postsId) => {
-    // fetch posts by id
-    const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${postsId}`).then(res => res.json())
+    let post
+    try {
+
+        // fetch posts by id
+        post = await fetch(`https://jsonplaceholder.typicode.com/posts/${postsId}`).then(res => {
+            if (res.status !== 200) {
+                throw new Error('Post not found')
+            }
+            return res.json()
+        })
+    } catch (err) {
+        // if post not found
+        document.getElementById('post').innerHTML = createNotFoundMessage()
+        return
+    }
+    
     // render post
     const postHTML = createPost(postsId, post.title, post.body)
     document.getElementById('post').innerHTML = postHTML
